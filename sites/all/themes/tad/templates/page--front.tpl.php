c60298b3b151f560dc5cb257e7a2f10d042493fd
vikranttechved <vikrant1001@techved.com>
2019-01-18 19:06:47 +0530

    Added CSS changes

--- sites/all/themes/tad/templates/page--front.tpl.php
+++ sites/all/themes/tad/templates/page--front.tpl.php
@@ -75,7 +75,7 @@
  */
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="<?php print $container_class; ?>">
+  <div class="<?php print $container_class; ?> flex">
     <div class="navbar-header">
       <?php if ($logo): ?>
         <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
